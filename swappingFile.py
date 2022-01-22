9d57f3969c45c97f364619c81ea983e534f74d60
Bookworm474 <76556656+Bookworm474@users.noreply.github.com>
2022-01-22 16:30:51 +0530

    Add files via upload

---
+++ swappingFile.py
@@ -0,0 +1,12 @@
+def swapFileData():
+    file1 = input("Enter the first file: ")
+    file2 = input("Enter the second file: ")
+    with open(file1, 'r') as a:
+        data_a = a.read()
+    with open(file2, 'r') as a:
+        data_b = a.read()
+    with open(file1, 'w') as a:
+        a.write(data_b)
+    with open(file2, 'w') as a:
+        a.write(data_a)
+swapFileData()
